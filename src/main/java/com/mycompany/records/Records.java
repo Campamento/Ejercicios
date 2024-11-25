2102541e581d361fd63770100881c44e4d305257
unknown <fernandaminga42@gmail.com>
2024-11-25 13:25:12 -0500

    Tarea

---
+++ src/main/java/com/mycompany/records/Records.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.records;
+
+/**
+ *
+ * @author Lizeth
+ */
+public class Records {
+
+    public static void main(String[] args) {
+        
+    }}
+        // Clase que representa un Producto en la tienda
